b1c1491b59d2d726cc5b6f3ef49472d489094270
shoaibakhtar9611 <shoibakhtar9611@gmail.com>
2020-04-17 20:07:06 +0530

    Updates

---
+++ precache-manifest.ebed689b00d4c8def539d4f10f599ea8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d846e07e9670ea3cd686732e1db3f65e",
+    "url": "/article-search-app/index.html"
+  },
+  {
+    "revision": "40aa1f96062f9e19de5c",
+    "url": "/article-search-app/static/css/main.5db1e149.chunk.css"
+  },
+  {
+    "revision": "987e4c51368248b11a1c",
+    "url": "/article-search-app/static/js/2.0234f12b.chunk.js"
+  },
+  {
+    "revision": "23ec6c7f0411a6141095a6c739b32980",
+    "url": "/article-search-app/static/js/2.0234f12b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "40aa1f96062f9e19de5c",
+    "url": "/article-search-app/static/js/main.2f686fc1.chunk.js"
+  },
+  {
+    "revision": "4d3444f8906c789dcd4e",
+    "url": "/article-search-app/static/js/runtime-main.ccef6937.js"
+  },
+  {
+    "revision": "6c645e7d6db1efcbb6f3bdfe24aeadf6",
+    "url": "/article-search-app/static/media/logo.6c645e7d.svg"
+  }
+]);
